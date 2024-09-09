e2b08911e9c7f423c8931b066266838e51786ab0
superp00t <superp00t@tutanota.com>
2024-09-08 21:09:01 -0400

    chore(profile): remove old 3.3.5a profile

--- 3.3.5a/include/storm/file.h
+++
@@ -1,9 +0,0 @@
-#ifndef STORM_FILE_H
-#define STORM_FILE_H
-
-DECLARE_STRUCT(SFile);
-
-struct SFile {
-};
-
-#endif
--- 3.3.5a/include/ui/simpleframe.h
+++
@@ -1,9 +0,0 @@
-#ifndef UI_SIMPLE_FRAME_H
-#define UI_SIMPLE_FRAME_H
-
-DECLARE_STRUCT(CSimpleFrame);
-
-struct CSimpleFrame {
-};
-
-#endif
