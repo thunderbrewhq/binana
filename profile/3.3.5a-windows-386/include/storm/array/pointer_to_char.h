9edded0c71995bb4cbc8758d0bdad1cdb0978abb
superp00t <superp00t@tutanota.com>
2026-04-29 03:07:52 -0400

    feat(profile): add char array

---
+++ profile/3.3.5a-windows-386/include/storm/array/pointer_to_char.h
@@ -0,0 +1,12 @@
+#ifndef STORM_ARRAY_POINTER_TO_CHAR_H
+#define STORM_ARRAY_POINTER_TO_CHAR_H
+
+#include "system/types.h"
+
+#include "storm/array.h"
+
+typedef char* pointer_to_char;
+
+STORM_TS_GROWABLE_ARRAY(pointer_to_char);
+
+#endif
