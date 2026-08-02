a5d6459a8b52d48a03c8bfd843b1b5980e36c7e4
superp00t <superp00t@tutanota.com>
2026-08-02 01:14:35 -0400

    refactor(common): COMMON_INSTANCE_* declarations now require external TSList declarations to avoid duplication

--- profile/3.3.5a-windows-386/include/common/instance.h
+++ profile/3.3.5a-windows-386/include/common/instance.h
@@ -6,43 +6,39 @@
 #include "storm/list.h"
 #include "storm/thread.h"
 
-// TSList<T>
-// TSLinkedNode<T>
 // TInstanceId<T>
 // TSingletonInstanceId<T>
-#define COMMON_INSTANCE_ID(T) \
-STORM_TS_LIST(T); \
-typedef struct TInstanceId_##T##__v_table TInstanceId_##T##__v_table; \
-typedef struct TInstanceId_##T TInstanceId_##T; \
-typedef struct TSingletonInstanceId_##T TSingletonInstanceId_##T; \
-struct TInstanceId_##T##__v_table { \
-  void* v_fn_00_scalar_deleting_destructor; \
-}; \
-struct TInstanceId_##T { \
-  TInstanceId_##T##__v_table* v_table; \
-  TSLinkedNode_##T b_base; \
-  uint32_t m_id; \
-}; \
-struct TSingletonInstanceId_##T { \
-  TInstanceId_##T b_base; \
-};
+#define COMMON_INSTANCE_ID(T)                                                                      \
+    typedef struct TInstanceId_##T##__v_table TInstanceId_##T##__v_table;                          \
+    typedef struct TInstanceId_##T            TInstanceId_##T;                                     \
+    typedef struct TSingletonInstanceId_##T   TSingletonInstanceId_##T;                            \
+    struct TInstanceId_##T##__v_table {                                                            \
+        void* v_fn_00_scalar_deleting_destructor;                                                  \
+    };                                                                                             \
+    struct TInstanceId_##T {                                                                       \
+        TInstanceId_##T##__v_table* v_table;                                                       \
+        TSLinkedNode_##T            b_base;                                                        \
+        uint32_t                    m_id;                                                          \
+    };                                                                                             \
+    struct TSingletonInstanceId_##T {                                                              \
+        TInstanceId_##T b_base;                                                                    \
+    };
 
-#define COMMON_INSTANCE_ID_TABLE(T) \
-STORM_TS_LIST(T); \
-typedef struct TInstanceIdTable_##T TInstanceIdTable_##T; \
-struct TInstanceIdTable_##T { \
-  SCritSect m_idCritSect; \
-  uint32_t m_id; \
-  int32_t m_idWrapped; \
-  CSRWLock m_idLock[8]; \
-  TSList_##T m_idList[8]; \
-};
+#define COMMON_INSTANCE_ID_TABLE(T)                                                                \
+    typedef struct TInstanceIdTable_##T TInstanceIdTable_##T;                                      \
+    struct TInstanceIdTable_##T {                                                                  \
+        SCritSect  m_idCritSect;                                                                   \
+        uint32_t   m_id;                                                                           \
+        int32_t    m_idWrapped;                                                                    \
+        CSRWLock   m_idLock[8];                                                                    \
+        TSList_##T m_idList[8];                                                                    \
+    };
 
 // template <class T>
 // class TExtraInstanceRecyclable
 typedef struct TExtraInstanceRecyclable TExtraInstanceRecyclable;
 struct TExtraInstanceRecyclable {
-  uint32_t m_recycleBytes;
+    uint32_t m_recycleBytes;
 };
 
 #endif
