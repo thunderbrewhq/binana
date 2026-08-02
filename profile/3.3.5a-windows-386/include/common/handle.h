41557edd697a6066cf77ac723db911dfa63510c1
superp00t <superp00t@tutanota.com>
2026-08-02 01:12:52 -0400

    refactor(common): DECLARE_HANDLE moved into detect.h

--- profile/3.3.5a-windows-386/include/common/handle.h
+++ profile/3.3.5a-windows-386/include/common/handle.h
@@ -3,17 +3,10 @@
 
 #include "system/types.h"
 
-#define DECLARE_HANDLE(I) \
-typedef struct I##__ I##__; \
-struct I##__ { \
-    int32_t unused; \
-}; \
-typedef I##__* I
-
 DECLARE_STRUCT(CHandleObject);
 
 struct CHandleObject {
-  int32_t m_refcount;
+    int32_t m_refcount;
 };
 
 DECLARE_HANDLE(HOBJECT);
